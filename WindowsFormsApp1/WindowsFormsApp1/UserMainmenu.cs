c5909ce94ce8d21a56024cb1ef22541ec009072a
Borshon Alfred Gomes <87852753+Borshon1017@users.noreply.github.com>
2022-11-25 18:34:18 +0600

    Usermain Menu

--- WindowsFormsApp1/WindowsFormsApp1/UserMainmenu.cs
+++ WindowsFormsApp1/WindowsFormsApp1/UserMainmenu.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class UserLogin : Form
+    public partial class UserMainmenu : Form
     {
-        public UserLogin()
+        public UserMainmenu()
         {
             InitializeComponent();
+        }
+
+        private void UserMainmenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
